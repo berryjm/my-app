fd184193ca52e47cbc6dc18116a802e167f7e41d
berryjm <jiminsberry@protonmail.com>
2022-10-23 22:07:26 -0400

    form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './component/home';
 import Nav from './component/nav';
+import Login from './component/login';
 
 class App extends Component {
   render() {
@@ -10,7 +11,8 @@ class App extends Component {
       <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="nav" element={<Nav />} />=
+        <Route path="nav" element={<Nav />} />
+        <Route path="login" element={<Login />} />
       </Routes>
     </Router>
    );
